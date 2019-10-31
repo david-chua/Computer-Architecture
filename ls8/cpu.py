e1c693bc9504ade8436dca09b9ff879bf6da1bcb
david-chua <dp_chua@hotmail.com>
2019-10-30 23:35:25 -0400

    started run

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -83,17 +83,8 @@ class CPU:
         """Run the CPU."""
         running = True
 
-        # while running:
-        #     ir = self.ram[self.pc]
-        #
-        #     operand_a = self.ram_read(self.pc + 1)
-        #     operand_b = self.ram_read(self.pc + 2)
-        #
-        #     try:
-        #         operation_output = self.commands[ir](operand_a, operand_b)
-        #         running = operation_output[1]
-        #         self.pc += operation_output[0]
-        #
-        #     except:
-        #         print(f"Unknown command: {ir}")
-        #         sys.exit()
+        while running:
+            ir = self.ram[self.pc]
+
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
